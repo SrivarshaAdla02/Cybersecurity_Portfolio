ef665207763454d4729348c20009002f6673dfff
Srivarsha Adla <adlavarsha02@gmail.com>
2025-06-07 17:08:24 -0400

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/avatar.svg"; // Replace with your custom avatar if needed
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -19,37 +19,29 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I am Srivarsha Adla, a passionate and detail-oriented cybersecurity enthusiast currently working as an{" "}
+              <b className="purple">Information Security Office Intern</b> at NYSED.
               <br />
-              <br />I am fluent in classics like
+              <br />
+              My areas of focus include:
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> threat detection, incident response, log analysis, and SIEM tools </b>
+              </i>
+              such as Splunk, CrowdStrike, and Nessus.
+              <br />
+              <br />
+              I enjoy solving real-world security problems and working with technologies like{" "}
+              <i>
+                <b className="purple">Linux, PowerShell, Wireshark, </b> and{" "}
+                <b className="purple">network security tools</b>.
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              I also bring experience in scripting and automation with{" "}
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+                <b className="purple">Python and Bash</b>
+              </i>{" "}
+              to streamline investigations and reduce response time.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
@@ -62,42 +54,42 @@ function Home2() {
           <Col md={12} className="home-about-social">
             <h1>FIND ME ON</h1>
             <p>
-              Feel free to <span className="purple">connect </span>with me
+              Feel free to <span className="purple">connect</span> with me
             </p>
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/SrivarshaAdla02"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <AiFillGithub />
                 </a>
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href="https://twitter.com/YourTwitterHandle"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <AiOutlineTwitter />
                 </a>
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/srivarsha-adla/"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <FaLinkedinIn />
                 </a>
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/YourInstaHandle"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
@@ -112,4 +104,5 @@ function Home2() {
     </Container>
   );
 }
+
 export default Home2;
